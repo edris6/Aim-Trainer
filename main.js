e21b4637243f55028d95f9e34e1debe44d4f3347
badonyt <soccerskin87@gmail.com>
2022-06-22 19:51:19 +0100

    Cookies update test

--- main.js
+++ main.js
@@ -15,24 +15,10 @@ let alwaystrue = true;
 let record;
 let alltimere
 
-function setCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
 function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
+	const value = `; ${document.cookie}`;
+	const parts = value.split(`; ${name}=`);
+	if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
 function setup() {
@@ -117,8 +103,7 @@ var timergame = setInterval(function () {
 			
 			record = getCookie("record");//"bobthegreat@gmail.com"
 			
-			console.log(err)
-			setCookie("record", alltimere, 31000); //set "user_email" cookie, expires in 30 days
+			document.cookie = "record=" + alltimere + "; expires=Thu, 18 Dec 2013 12:00:00 UTC"; 
 			
 		}
 		clickedprs = clicked;
